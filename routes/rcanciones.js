d6d37c7797e19244e21d856e0cb300897cd959f8
Nut <nutbanderag@gmail.com>
2020-03-15 19:22:54 +0100

    SDI-NodeJS- 8,9,10 Recursos estáticos, Peticiones POST y parámetros Enrutamiento y comodines

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -8,19 +8,29 @@ module.exports = function (app) {
         res.send(respuesta);
     });
 
-    app.get('/suma', function(req, res) {
+    app.get('/suma', function (req, res) {
         var respuesta = parseInt(req.query.num1) + parseInt(req.query.num2);
         res.send(String(respuesta));
     });
 
-    app.get('/canciones/:id', function(req, res) {
+    app.get('/canciones/:id', function (req, res) {
         var respuesta = 'id: ' + req.params.id;
         res.send(respuesta);
     });
 
-    app.get('/canciones/:genero/:id', function(req, res) {
+    app.get('/canciones/:genero/:id', function (req, res) {
         var respuesta = 'id: ' + req.params.id + '<br>'
             + 'Género: ' + req.params.genero;
         res.send(respuesta);
+    });
+
+    app.post("/cancion", function (req, res) {
+        res.send("Canción agregada: " + req.body.nombre + "<br>"
+            + "género: " + req.body.genero + "<br>"
+            + "precio: " + req.body.precio);
+    });
+
+    app.get('/promo*', function (req, res) {
+        res.send('Respuesta patrón promo* ');
     });
 };
