907ac140db7baae16f20e406655450811582c33d
Nut <nutbanderag@gmail.com>
2020-03-20 18:50:27 +0100

    SDI-NodeJS-8.5,67- Registro de usuario, autenticación y uso de sesión

--- app.js
+++ app.js
@@ -2,20 +2,32 @@
 var express = require('express');
 var app = express();
 
-let fileUpload = require('express-fileupload'); app.use(fileUpload());
+let expressSession = require('express-session');
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+
+let crypto = require('crypto');
+let fileUpload = require('express-fileupload');
+app.use(fileUpload());
 let mongo = require('mongodb');
 var swig = require('swig');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-let gestorBD = require("./modules/gestorBD.js"); gestorBD.init(app,mongo);
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app, mongo);
 
 
 app.use(express.static('public'));
 
 // Variables
 app.set('port', 8081);
-app.set('db','mongodb://admin:sdilabnode901@tiendamusica-shard-00-00-xbeyo.mongodb.net:27017,tiendamusica-shard-00-01-xbeyo.mongodb.net:27017,tiendamusica-shard-00-02-xbeyo.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority');
+app.set('db', 'mongodb://admin:sdilabnode901@tiendamusica-shard-00-00-xbeyo.mongodb.net:27017,tiendamusica-shard-00-01-xbeyo.mongodb.net:27017,tiendamusica-shard-00-02-xbeyo.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority');
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
 
 // Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
